8e0441d3e33c2a1222019581301791951a434cea
GENTLEW1ND <rajchakraborty029@gmail.com>
2023-06-25 15:36:38 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return <nav>
+    <h1>TechStar</h1>
+    <main>
+      <Link to="/">Home</Link>
+      <Link to="/Contact">Contact</Link>
+      <Link to="/#About">About</Link>
+      <Link to="/#Brands">Brands</Link>
+      <Link to="/#Services">Services</Link>
+    </main>
+  </nav>
+}
+
+export default Header
